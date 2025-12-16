66eb0456e4ab834a5e7004c9aadd262bede87fb3
fabziano <fabianotmp@gmail.com>
2025-12-15 22:07:15 -0300

    mentoria

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,18 @@ public class Main {
         curso2.setDescricao("descrição curso JS");
         curso2.setCargaHoraria(4);
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria de Java");
-        mentoria.setDescricao("Descricao mentoria Java");
-        mentoria.setData(LocalDate.now());
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descricao mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria de JS");
+        mentoria2.setDescricao("Descricao mentoria JS");
+        mentoria2.setData(LocalDate.now());
 
         System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria2);
     }
 }
